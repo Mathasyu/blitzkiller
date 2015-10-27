918a048591717760ea6018a834eaf70fe3820f45
Mathasyu <test@tmc2000.de>
2015-10-27 19:54:31 +0100

    Pizza-Algorithmus

--- main.c
+++ main.c
@@ -17,35 +17,29 @@ Temparatur des Ofens int ofentemparatur
 KERN-ALGORITHMUS
 Herd auf 200° vorheizen (Ofenobjekt)
 Prüfe Temparatur des Ofens
-FALLS ofentemparatur >= 200
+FALLS (ofentemparatur >= 200)
 DANN
-Pizza aus Folie nehmen und auf das Backblech legen
-Einlesen der Garzeit
+    Pizza aus Folie nehmen und auf das Backblech legen
+    Einlesen der Garzeit
 ENDE FALLS
 
 WIEDERHOLE
-Backe Pizza
-SOLANGE
-Garzeit < angegebene Garzeit
-ENDE SOLANGE
+    Backe Pizza
+SOLANGE (Garzeit < angegebene Garzeit)
 
 WIEDERHOLE
     Prüfe, ob die Pizza gut ist
     Einlesen Pizzastatus
-    FALLS Pizzastatus == nicht OK
+
+    FALLS (Pizzastatus == nicht OK)
     DANN
-    Einlesen der weiteren Backzeit
+        Einlesen der weiteren Backzeit
 
-    WIEDERHOLE
-    Backe Pizza
-    SOLANGE
-    Garzeit < weiteren Garzeit
-    ENDE SOLANGE
+        WIEDERHOLE
+            Backe Pizza
+        SOLANGE (Garzeit < weiteren Garzeit)
     ENDE FALLS
-SOLANGE
-Pizzastatus == nicht OK
-ENDE SOLANGE
-
+SOLANGE (Pizzastatus == nicht OK)
 
 Pizza aus Ofen nehmen
 Pizza auf Teller legen
